deb4205b6ac9d859930d2a022eba5022ef5c5f2d
ColinEnck <colinenck@gmail.com>
2023-03-12 13:11:24 -0400

    finalish commit for pong

--- Pong/pong.cpp
+++ Pong/pong.cpp
@@ -1,20 +1,15 @@
 #include <SFML/Graphics.hpp>
 
+const float BALL_DV = 3.125 * 3.125;
+const float PADDLE_DV = 1.9 * 3.125;
 const int WIN_X_LEN = 800;
 const int WIN_Y_LEN = 600;
 
 void ball_reset(sf::CircleShape &ball, sf::Vector2f &ball_v) 
 {
 	ball.setPosition(WIN_X_LEN / 2, WIN_Y_LEN / 7);
-    
-    if (1) // TODO: change the 1 to a scoring system so the winner goes first
-    {
-    	ball_v.x = 3.125 * 3.125;
-    	ball_v.y = 3.125 * 3.125;
-    } else {
-    	ball_v.x = -3.125 * 3.125;
-    	ball_v.y = 3.125 * 3.125;
-    }
+    ball_v.x = -BALL_DV;
+    ball_v.y = BALL_DV;
 }
 
 int main()
@@ -49,46 +44,48 @@ int main()
 
         // moves paddles based on keyboard input
         if (sf::Keyboard::isKeyPressed(sf::Keyboard::W) && 
-				l_Rect.getPosition().y >= 0)
-    		l_Rect.move(0, -1.9 * 3.125);
+			l_Rect.getPosition().y >= 0)
+    		l_Rect.move(0, -(PADDLE_DV));
 
     	else if (sf::Keyboard::isKeyPressed(sf::Keyboard::S) && 
-				l_Rect.getPosition().y <= WIN_Y_LEN - 100)
-       		l_Rect.move(0, 1.9 * 3.125);
+			l_Rect.getPosition().y <= WIN_Y_LEN - 100)
+       		l_Rect.move(0, PADDLE_DV);
 
 		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up) && 
-				r_Rect.getPosition().y >= 0)
-        	r_Rect.move(0, -1.9 * 3.125);
+			r_Rect.getPosition().y >= 0)
+        	r_Rect.move(0, -(PADDLE_DV));
 
    		else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Down) && 
-				r_Rect.getPosition().y <= WIN_Y_LEN - 100)
-        	r_Rect.move(0, 1.9 * 3.125);
+			r_Rect.getPosition().y <= WIN_Y_LEN - 100)
+        	r_Rect.move(0, PADDLE_DV);
+
+        // ball-paddle logic
+        // left paddle vvv
+        if (ball.getPosition().x <= 10)
+        {
+           if (ball.getPosition().y >= l_Rect.getPosition().y &&
+            ball.getPosition().y <= (l_Rect.getPosition().y +
+                l_Rect.getSize().y))
+            ball_v.x = -(ball_v.x);
+        }
+        // right paddle vvv
+        if (ball.getPosition().x >= WIN_X_LEN - 35)
+        {
+            if (ball.getPosition().y >= r_Rect.getPosition().y &&
+            ball.getPosition().y <= (r_Rect.getPosition().y +
+                r_Rect.getSize().y))
+            ball_v.x = -(ball_v.x);
+        }
 
     	// ball-wall logic
         if (ball.getPosition().y >= WIN_Y_LEN)
-        	ball_v.y = 0 - ball_v.y;
+        	ball_v.y = -(ball_v.y);
         else if (ball.getPosition().y <= 0)
-        	ball_v.y = 0 - ball_v.y;
+        	ball_v.y = -(ball_v.y);
         else if (ball.getPosition().x >= WIN_X_LEN)
         	ball_reset(ball, ball_v);
-        	// ball_v.x = 0 - ball_v.x; **for endless mode**
         else if (ball.getPosition().x <= 0)
         	ball_reset(ball, ball_v);
-        	// ball_v.x = 0 - ball_v.x; **for endless mode**
-
-        // ball-paddle logic
-        if (ball.getPosition().x <= (l_Rect.getPosition().x + 
-        		l_Rect.getSize().x) && ball.getPosition().y >=
-        		l_Rect.getPosition().y && ball.getPosition().y <=
-        		(l_Rect.getPosition().y + l_Rect.getSize().y))
-        	ball_v.x = 0 - ball_v.x;
-        // ^^ left paddle **fix basic physics**
-		if (ball.getPosition().x >= r_Rect.getPosition().x && 
-				ball.getPosition().y >= r_Rect.getPosition().y 
-				&& ball.getPosition().y <= (r_Rect.getPosition().y 
-				+ r_Rect.getSize().y))
-        	ball_v.x = 0 - ball_v.x;
-        // ^^ right paddle **fix basic physics**
 
         // moves the ball with the updated velocity
     	ball.move(ball_v.x, ball_v.y);
