d7e7eb547ba2305d2966c2bc4d92fd32b3a705fc
ColinEnck <colinenck@gmail.com>
2023-04-07 22:58:46 -0400

    I'm done for now. Phew

--- FlappyBird/bird.cpp
+++ FlappyBird/bird.cpp
@@ -1,8 +1,25 @@
 #include <SFML/Graphics.hpp>
+#include <stdlib.h>
+#include <time.h>
+
+// GUIDE FOR PIPES PORPORTIONS
+//  7        7
+// --> pipe <--
+//      |
+//      |28
+//      |
+//      V
+// --> pipe <--
+// 
+// pipes are 66px long scaled to 528px
+// 28px space between them scaled to 224px
+// drawing is 25x160 px scaled to 200x1280 px
+
+// bird is 16x16 (with 3px and 1px padding for x and y) scaled to 128x128 px
 
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode(800, 640), "Bird Game - Colin Enck");
+    sf::RenderWindow window(sf::VideoMode(600, 640), "Bird Game - Colin Enck");
     window.setFramerateLimit(60);
 
     sf::Texture backdrop;
@@ -12,21 +29,29 @@ int main()
     background.setTexture(backdrop);
     background.setScale(sf::Vector2f(8.f, 8.f));
 
-    sf::Texture bird1;
-    if (!bird1.loadFromFile("bird1.png"))
-        return 1;
-    sf::Texture bird2;
-    if (!bird2.loadFromFile("bird2.png"))
+    sf::Texture birdTex;
+    if (!birdTex.loadFromFile("bird.png"))
         return 1;
     sf::Sprite bird;
-    bird.setTexture(bird1);
+    bird.setTexture(birdTex);
     bird.setScale(sf::Vector2f(8.f, 8.f));
-    bird.setPosition(sf::Vector2f(267.f - 64, 213.f - 64));
+    bird.setPosition(sf::Vector2f(203.f, 149.f));
+
+    sf::Texture pipesTex;
+    if (!pipesTex.loadFromFile("pipes.png"))
+        return 1;
+    sf::Sprite pipes;
+    pipes.setTexture(pipesTex);
+    pipes.setScale(sf::Vector2f(8.f, 8.f));
+    pipes.setPosition(sf::Vector2f(600.f, -325.f));
 
     sf::Clock clock;
     sf::Time dt;
 
     float velocity = 0;
+
+    bool inTubeX;
+    bool touchTube;
 
     while (window.isOpen())
     {
@@ -37,21 +62,47 @@ int main()
                 window.close();
         }
 
-        dt = clock.restart();
-	velocity -= dt.asMilliseconds();
+        srand(time(NULL));
 
-        // keyboard inputs
+        // clock stuff
+        dt = clock.restart();
+        velocity -= dt.asMilliseconds();
+
+        // handles input
         if (sf::Keyboard::isKeyPressed(sf::Keyboard::Escape))
             window.close();
         if (sf::Keyboard::isKeyPressed(sf::Keyboard::Space))
             velocity += 3.125 * 19;
 
+        // moves sprites
         bird.move(sf::Vector2f(0.f, -(velocity * dt.asSeconds())));
+        pipes.move(sf::Vector2f(-200.f * dt.asSeconds(), 0.f));
+        if (pipes.getPosition().x <= 178)
+        {
+            // -475 < y < -175
+            int range = -(175) + 475 + 1;
+            float num = rand() % range - 475;
+            pipes.setPosition(sf::Vector2f(600.f, num));
+        }
+
+        // handles game over conditions
+        if (bird.getPosition().y <= -0 || bird.getPosition().y >= 512)
+            window.close();
+
+        inTubeX = (203 >= (pipes.getPosition().x + 10)) &&\
+        (203 <= (pipes.getPosition().x + 197));
+
+        touchTube = (bird.getPosition().y <= (pipes.getPosition().y + 528))\
+        || (bird.getPosition().y >= (pipes.getPosition().y + 752));
+
+        if (inTubeX && touchTube)
+            window.close();
 
         // drawing begins here
         window.clear();
         window.draw(background);
         window.draw(bird);
+        window.draw(pipes);
         window.display();
     }
 
