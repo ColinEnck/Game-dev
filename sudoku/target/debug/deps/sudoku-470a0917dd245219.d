43ad640d0a43c96dba06caa85938c21eae36cd12
ColinEnck <colinenck@gmail.com>
2023-12-14 10:17:04 -0500

    .

---
+++ sudoku/target/debug/deps/sudoku-470a0917dd245219.d
@@ -0,0 +1,5 @@
+/home/colinenck/CS/repos/Game-dev/sudoku/target/debug/deps/sudoku-470a0917dd245219.rmeta: src/main.rs
+
+/home/colinenck/CS/repos/Game-dev/sudoku/target/debug/deps/sudoku-470a0917dd245219.d: src/main.rs
+
+src/main.rs:
